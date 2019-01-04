07d19f7334dafd05ad00c348e7a4a06d2baef0c8
That_Guy <eghove@gmail.com>
2019-01-04 16:40:26 -0600

    base styling for index.handlebars, serving up static content

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 8080;
 // initiate express
 const app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+
 // middleware to deal with JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
