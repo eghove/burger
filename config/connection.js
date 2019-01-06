f7296f3415c281d691d8a9bfa5db5e8f1731f481
That_Guy <eghove@gmail.com>
2019-01-06 13:05:43 -0600

    getting it ready to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -9,22 +9,34 @@ const keys = require("./keys.js");
 let mysql = require("mysql");
 
 
+
+
 //==THE DATABASE CONNECTION=========================================
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: keys.mySQL.secret,
-    database: "burgers_db"
-});
+// variable to store the connection
+let connection;
+
+// if deployed to heroku, use JAWS DB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    // if deployed locally, use local database
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: keys.mySQL.secret,
+        database: "burgers_db"
+    });
+}
+
 
 // TODO: I'll need to refactor this to allow for jawsDB use on HEROKU
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
-    } 
+    }
     console.log("connected as id: " + connection.threadId);
 });
 
