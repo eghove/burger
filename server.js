52e6a95f7ad4720e9fac5e2d4f3415e3c3ead7d6
That_Guy <eghove@gmail.com>
2019-01-27 16:29:57 -0600

    linted server.js

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
+/* eslint-disable semi */
 // REQUIREMENTS
 
 // Require express
-const express = require("express");
+var express = require('express');
 
 // set up the port
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 // initiate express
-const app = express();
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
+app.use(express.static('public'));
 
 // middleware to deal with JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // SET HANDLEBARS as a variable called hbars
-const hbars = require("express-handlebars");
+var hbars = require('express-handlebars');
 
 // initiate handlebars, tell it where to find the main.handlebars page
-app.engine("handlebars", hbars ({ defaultLayout: "main" }));
+app.engine('handlebars', hbars({ defaultLayout: 'main' }));
 
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 // IMPORT ROUTES
-const routes = require("./controllers/burgers_controller.js");
+var routes = require('./controllers/burgers_controller.js');
 
 // give the server access to the routes
 app.use(routes);
 
 // START THE SERVER
 app.listen(PORT, function () {
-    // log server side when the server starts
-    console.log("Server listening on: http://localhost:" + PORT);
+  // log server side when the server starts
+  console.log('Server listening on: http://localhost:' + PORT);
 })
-
