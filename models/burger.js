16da2e11e7e82164a4ff0215e693cd9f156743d8
That_Guy <eghove@gmail.com>
2019-01-27 16:34:46 -0600

    linted burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,32 +1,32 @@
+/* eslint-disable semi */
 // IMPORT THE ORM
-let orm = require("../config/orm.js");
+var orm = require('../config/orm.js');
 
 // burger model object
 
-let burger = {
+var burger = {
 
-    // function to display burgers
-    displayBurgers: function(callBack) {
-        orm.selectAll("burgers", function(res) {
-            callBack(res);
-        });
-    },
+  // function to display burgers
+  displayBurgers: function (callBack) {
+    orm.selectAll('burgers', function (res) {
+      callBack(res);
+    });
+  },
 
-    // function to add burgers
-    addBurger: function(columns, values, callBack) {
-        orm.insertOne("burgers", columns, values, function(res) {
-            callBack(res);
-        });
-    },
+  // function to add burgers
+  addBurger: function (columns, values, callBack) {
+    orm.insertOne('burgers', columns, values, function (res) {
+      callBack(res);
+    });
+  },
 
-    // function to devour a burger
-    devourBurger: function(columnToChange, valueToChange, condition, callBack) {
-        orm.updateOne("burgers", columnToChange, valueToChange, condition, function(res) {
-            callBack(res);
-        });
-    }
+  // function to devour a burger
+  devourBurger: function (columnToChange, valueToChange, condition, callBack) {
+    orm.updateOne('burgers', columnToChange, valueToChange, condition, function (res) {
+      callBack(res);
+    });
+  }
 }
 
 // export the burger model
-
-module.exports = burger; 
+module.exports = burger;
