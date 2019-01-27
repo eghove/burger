79214b8c8e443b085197d253da50398e6e494196
That_Guy <eghove@gmail.com>
2019-01-27 16:41:28 -0600

    linted connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,43 +1,38 @@
+/* eslint-disable semi */
 // ===REQUIREMENTS==================================================
-//require the dotenv module
-require("dotenv").config();
+// require the dotenv module
+require('dotenv').config();
 
-//loads the exported pieces from keys.js
-const keys = require("./keys.js");
+// loads the exported pieces from keys.js
+var keys = require('./keys.js');
 
 // import mysql library
-let mysql = require("mysql");
+var mysql = require('mysql');
 
-
-
-
-//==THE DATABASE CONNECTION=========================================
+// ==THE DATABASE CONNECTION=========================================
 // variable to store the connection
-let connection;
+var connection;
 
 // if deployed to heroku, use JAWS DB
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-    // if deployed locally, use local database
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: keys.mySQL.secret,
-        database: "burgers_db"
-    });
+  // if deployed locally, use local database
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: keys.mySQL.secret,
+    database: 'burgers_db'
+  });
 }
 
-
-// TODO: I'll need to refactor this to allow for jawsDB use on HEROKU
-
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id: " + connection.threadId);
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id: ' + connection.threadId);
 });
 
 // export the connection
